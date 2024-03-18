aee5c33f9d129f8222c3833e2477a50a0618fbc6
jocelyn <jocelynnecio78@gmail.com>
2024-03-18 13:09:40 +0800

    fix: merge conflict

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,21 +1,3 @@
-<<<<<<< Updated upstream
-import VideoCard from "@/components/molecules/Card/VideoCard";
-import Navbar from "@/components/molecules/Navbar/Navbar";
-import { videos } from "@/lib/api/data";
-
-export default function Home() {
-  const video = videos[0]
-
-  return (
-    <main className="flex min-h-screen bg-white dark:bg-yt-black flex-col items-center">
-      <Navbar />
-      <VideoCard
-        title="NextJS Crash Course"
-        image="https://i.ytimg.com/vi/ZjAqacIC_3c/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBc3blSmtVGX4TsNLvj385ak-6MxQ"
-        author={author}
-        views="51M"
-      />
-=======
 import VideoCard from "@/components/molecules/Card/VideoCard";
 import Navbar from "@/components/molecules/AppBar/AppBar";
 import { videos } from "@/lib/api/data";
@@ -27,7 +9,6 @@ export default function Home() {
     <main className="flex min-h-screen bg-white dark:bg-yt-black flex-col items-center">
       <Navbar />
       <VideoCard details={video} />
->>>>>>> Stashed changes
     </main>
   );
 }
