849cd69f14e3899216eac02eaa40b4e695db2a5a
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-18 21:04:45 +0800

    feat: added shorts heading

--- client/components/molecules/List/ShortsList.tsx
+++ client/components/molecules/List/ShortsList.tsx
@@ -1,19 +1,27 @@
 import FlatList from "@/components/atoms/List/FlatList";
 import ShortsCard from "../Card/ShortsCard";
 import { videos } from "@/lib/api/data";
+import { SiYoutubeshorts } from "react-icons/si";
 
 const ShortsList = () => {
   return (
-    <div
-      className="flex gap-4 py-4 overflow-x-auto w-full my-9"
-      style={{ scrollbarWidth: "none" }}
-    >
-      <FlatList
-        data={videos}
-        keyExtractor={(item) => item.id}
-        RenderItem={(item) => <ShortsCard details={item} />}
-      />
-    </div>
+    <>
+      <div className="w-full flex gap-2 items-center p-4">
+        <SiYoutubeshorts size={30} color="red"/>
+        <h2 className="text-xl text-yt-black font-medium">Shorts</h2>
+      </div>
+
+      <div
+        className="flex gap-4 overflow-x-auto w-full mb-9"
+        style={{ scrollbarWidth: "none" }}
+      >
+        <FlatList
+          data={videos}
+          keyExtractor={(item) => item.id}
+          RenderItem={(item) => <ShortsCard details={item} />}
+        />
+      </div>
+    </>
   );
 };
 
