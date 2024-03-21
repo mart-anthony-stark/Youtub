915734e62ed0085514ff869cae6432965cb7d2ac
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-21 21:36:42 +0800

    fix: added active category state

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,6 +4,8 @@ import CategoryList from "@/components/molecules/List/CategoryList";
 import VideoList from "@/components/molecules/List/VideoList";
 import Shorts from "@/components/organisms/Shorts/Shorts";
 import MainTemplate from "@/components/templates/MainTemplate";
+import SkeletonLoader from "@/components/molecules/Card/SkeletonLoader";
+import FlatList from "@/components/atoms/List/FlatList";
 
 export default function Home() {
   return (
@@ -13,7 +15,14 @@ export default function Home() {
       </div>
       <div className="pt-12">
         <VideoList />
-        <Shorts className="pl-4 sm:pl-2 lg:pl-2 xl:pl-8"/>
+        <Shorts className="pl-4 sm:pl-2 lg:pl-2 xl:pl-8" />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between gap-2 xl:pl-8">
+          <FlatList
+            data={Array(6).fill(0)}
+            keyExtractor={(item, index) => `${item}-${index}`}
+            RenderItem={(item) => <SkeletonLoader />}
+          />
+        </div>
       </div>
     </MainTemplate>
   );
