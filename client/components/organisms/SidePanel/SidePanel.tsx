11d5ad6f2eb3b165d81396679ebd0f0f8b6ae961
jocelyn <jocelynnecio78@gmail.com>
2024-03-21 16:14:24 +0800

    hotfix/added sidepanel-bottom

--- client/components/organisms/SidePanel/SidePanel.tsx
+++ client/components/organisms/SidePanel/SidePanel.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Button from "@/components/atoms/Button/Button";
 import MenuButton from "@/components/atoms/Button/MenuButton";
@@ -66,14 +66,19 @@ const channels: Channel[] = [
 ];
 
 const SidePanel = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <>
       <div
         className="hidden w-16 fixed top-[60px] pt-2 left-0 z-50 scroll md:flex flex-col overflow-y-auto xl:w-60 px-2 bg-white"
         style={{ height: "calc(100vh - 60px)" }}
       >
-        <MenuButton onClick={() => router.push('/')} text="Home" Icon={<GoHomeFill size={24} />} active />
+        <MenuButton
+          onClick={() => router.push("/")}
+          text="Home"
+          Icon={<GoHomeFill size={24} />}
+          active
+        />
         <MenuButton
           text="Shorts"
           Icon={<SiYoutubeshorts size={24} color="red" />}
@@ -166,30 +171,43 @@ const SidePanel = () => {
 
         <hr className="mt-4" />
 
-        <div className="flex flex-wrap text-gray-700 font-serif text-sm p-3 gap-1">
-          <Link href={"https://about.youtube/"}>About</Link>
-          <Link href={"https://blog.youtube/"}>Press</Link>
-          <Link
-            href={"https://www.youtube.com/howyoutubeworks/policies/copyright/"}
-          >
-            Copyright
-          </Link>
-          <Link href={"https://www.youtube.com/t/contact_us/"}>
-            Contact us{" "}
-          </Link>
-          <Link href={"https://www.youtube.com/ads/"}>Advertise</Link>
-          <Link href={"https://developers.google.com/youtube"}>Developers</Link>
-          <Link href={"https://www.youtube.com/t/terms"}>Terms</Link>
-          <Link href={"https://policies.google.com/privacy?hl=en"}>
-            Privacy
-          </Link>
-          <Link
-            href={
-              "https://www.youtube.com/howyoutubeworks/policies/community-guidelines/"
-            }
-          >
-            Policy & Safety
-          </Link>
+        <div className="flex flex-wrap text-gray-700 text-xs p-3 gap-1">
+          <div className="flex flex-row gap-1">
+            <Link href={"https://about.youtube/"}>About</Link>
+            <Link href={"https://blog.youtube/"}>Press</Link>
+            <Link
+              href={
+                "https://www.youtube.com/howyoutubeworks/policies/copyright/"
+              }
+            >
+              Copyright
+            </Link>
+          </div>
+          <div>
+            <Link href={"https://www.youtube.com/t/contact_us/"}>
+              Contact us{" "}
+            </Link>
+            <Link href={"https://www.youtube.com/ads/"}></Link>Creators
+          </div>
+          <div>
+            <Link href={"https://www.youtube.com/ads/"}>Advertise</Link>
+            <Link href={"https://developers.google.com/youtube"}>
+              Developers
+            </Link>
+          </div>
+          <div className="pt-4 gap-2">
+            <Link href={"https://www.youtube.com/t/terms"}>Terms</Link>
+            <Link href={"https://policies.google.com/privacy?hl=en"}>
+              Privacy
+            </Link>
+            <Link
+              href={
+                "https://www.youtube.com/howyoutubeworks/policies/community-guidelines/"
+              }
+            >
+              Policy & Safety
+            </Link>
+          </div>
           <Link
             href={
               "https://www.youtube.com/howyoutubeworks/?utm_campaign=ytgen&utm_source=ythp&utm_medium=LeftNav&utm_content=txt&u=https%3A%2F%2Fwww.youtube.com%2Fhowyoutubeworks%3Futm_source%3Dythp%26utm_medium%3DLeftNav%26utm_campaign%3Dytgen"
@@ -199,7 +217,7 @@ const SidePanel = () => {
           </Link>
           <Link href={"https://www.youtube.com/new"}>Test new features</Link>
         </div>
-        <p className="text-gray-400 pl-4 text-sm mt-1"> © 2024 Google LLC</p>
+        <p className="text-gray-400 pl-4 text-xs mt-1"> © 2024 Google LLC</p>
       </div>
 
       <div
